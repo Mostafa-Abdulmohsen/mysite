98e9c159c913ea6ccfc47e96ea86ec0b290e9950
moustafa160336@fci.bu.edu.eg <moustafa160336@fci.bu.edu.eg>
2020-06-07 22:56:42 +0200

    django

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render(request,'index.html',{title:home})
+    return render(request,'blog/index.html',{})
